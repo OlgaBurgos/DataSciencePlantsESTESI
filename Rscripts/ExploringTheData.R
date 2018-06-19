7b330b4864cc8d80888afe608275eb74e062341a
Erola Fenollosa <31477298+erolafr@users.noreply.github.com>
2018-06-19 19:42:28 +0200

    Update ExploringTheData.R

--- Rscripts/ExploringTheData.R
+++ Rscripts/ExploringTheData.R
@@ -32,7 +32,21 @@ plot(pcabIO$x[,1],pcabIO$x[,2])
 text(pcabIO$x[,1],pcabIO$x[,2], Data$genus, cex=0.7, pos=4, col="red")
 
 
+install.packages("factoextra")
+library("factoextra")
+
 plot(pcabIO$x[,1],pcabIO$x[,2], col=Data$genus)
 
+fviz_pca_ind(pcabIO,
+             geom.ind = "point", # show points only (nbut not "text")
+             col.ind = Data$genus, # color by groups
+             palette = "Spectral",
+             addEllipses = TRUE, # Concentration ellipses
+             legend.title = "Groups")
+
+
+
+
+#http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/112-pca-principal-component-analysis-essentials/
 
 
