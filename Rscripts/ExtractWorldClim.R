0a5e08a4b96b1a0ee564aa17844a0c6fe678e8b2
geo.aules <geo.aules@geommaules-PC.ub.edu>
2018-06-19 16:27:58 +0200

    añado lineas y guardo los datos en Data

--- Rscripts/ExtractWorldClim.R
+++ Rscripts/ExtractWorldClim.R
@@ -1,6 +1,7 @@
 ## Como extraer los datos de WorldClim para las coordenadas seleccionadas.
 
 #Cargamos los datos del GBIF, que incluyen las coordenadas
+install.packages("readr")
 library(readr)
 gbif <- read_delim("0022664-180508205500799.csv", 
                    "\t", escape_double = FALSE, trim_ws = TRUE)
@@ -9,9 +10,9 @@ View(gbif)
 cord <- data.frame (lon = gbif$decimallongitude, lat=gbif$decimallatitude) #Df con solo las coordenadas
 
 install.packages("dismo")
-library ("dismo")
 library(raster)
 library(sp)
+library ("dismo")
 
 r2.5 <- getData("worldclim",var="bio",res=2.5)
 names(r2.5) <- c("BIO1","BIO2","BIO3","BIO4", "BIO5", "BIO6", "BIO7", "BIO8", "BIO9",
@@ -29,3 +30,6 @@ df <- cbind.data.frame(coordinates(points),values) #df con las coordenadas y sus
 View(df)
 summary(df) #Hay muchos NA, hay que explorar porque. 
 
+Data <- df[complete.cases(df),]
+
+write.csv(Data, file = "Data.csv")
