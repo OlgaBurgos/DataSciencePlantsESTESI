9177e08a9f2fe217fd7e74a8d303b42c0cb0c38d
Olga Burgos <olguiburgos@gmail.com>
2018-07-04 20:46:22 +0200

    Analisis Exploratorio a partir de Dataset MarcoClim10). Comenzando...

---
+++ SpanishRiskAssesment/AnalisisExploratorio.R
@@ -0,0 +1,31 @@
+
+#ANALISIS EXPLORATORIO. 
+install.packages("ggplot2")
+library("ggplot2")
+install.packages("DataExplorer")
+library(DataExplorer)
+head (MarcoClim10)
+mode (MarcoClim10)
+
+dim(MarcoClim10) #for checking the dimension
+plot(MarcoClim10)  
+plot_str(MarcoClim10) #visualizar estructura de los datos
+is.na(MarcoClim10) #¿Tengo muchos datos faltantes? Entonces veremos cuales son las variables más importantes
+plot_missing(MarcoClim10) #gráfico con missing values
+plot_histogram(MarcoClim10)
+
+plot_density(MarcoClim10)#This function visualizes density estimates for each continuous feature.
+
+
+##Anlalisis Exploratorio Multivariante##
+
+plot_correlation(MarcoClim10, 
+                 type = "continuous") #Nose si esta bien escrito
+
+
+install.packages("leaflet") #crear mapas
+library(leaflet)
+install.packages("ppcor") #correlacion entre variables
+library(ppcor)
+
+#Reduccion de la dimension. PCA, LDA? 
