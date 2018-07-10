6a25974cd1344cd4d8ba38dd07d66586e2ab0266
Ramon Diaz <jrbr_6@hotmail.com>
2018-07-10 10:56:34 +0200

    He intentado graficarlo de otra manera con una función que no habíamos usado hasta ahora, pero visualmente no queda muy bonito, demasiados puntos!

--- SpanishRiskAssesment/PCAa.R
+++ SpanishRiskAssesment/PCAa.R
@@ -17,15 +17,30 @@ summary(pcaclim)
 pcaclim$rotation[,1]
 pcaclim$rotation[,2]
 pcaclim$rotation[,3]
-plot(pcaclim$x[,1],pcaclim$x[,13])
-text(pcaclim$x[,3],pcaclim$x[,13], col="blue")
+plot(pcaclim$x[,1],pcaclim$x[,2])
+
 
 fviz_pca_var(pcaclim,
              col.var = "contrib", # Color by contributions to the PC
              gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
              repel = TRUE     # Avoid text overlapping
-)
+)  ##Función para ver la contribución de cada variable en esta PCA
 
+## Manera de representar la PCA extraído de https://tgmstat.wordpress.com/2013/11/28/computing-and-visualizing-pca-in-r/
+library(devtools)
+install_github("ggbiplot", "vqv")
+
+library(ggbiplot)
+?ggbiplot
+g <- ggbiplot(pcaclim, obs.scale = 1, var.scale = 1, 
+               ellipse = TRUE, alpha=0.1, 
+              circle = TRUE)
+g <- g + scale_color_discrete(name = '')
+g <- g + theme(legend.direction = 'horizontal', 
+               legend.position = 'top')
+print(g)
+
+##Aquí PCA con con las variables de VIF<5 
 marcoclimVIF <- marcoclim[,c(2,3,8,9,13,15)]
 
 pcaclim2 <- prcomp(marcoclimVIF, center = TRUE, scale. = TRUE) 
